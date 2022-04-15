a7dd91cd85446bc854a862c4e567f1d1e26952bd
LUPOSaymon <simone.mija@gmail.com>
2022-04-15 21:22:33 +0200

    Adding another section

--- resources/jquery/index.js
+++ resources/jquery/index.js
@@ -20,4 +20,25 @@ $(window).scroll(function () {
         $('#our-project-title-id').addClass('our-project-title-animation');
         $('#our-project-logo-id').addClass('our-project-logo-animation');
     }
+    else if (!isScrolledIntoView($('#our-project-text-id')))
+    {
+        //TODO:fix it
+        /*
+        $('#our-project-text-id').removeClass('our-project-text-animation');
+        $('#our-project-title-id').removeClass('our-project-title-animation');
+        $('#our-project-logo-id').removeClass('our-project-logo-animation');
+        $('#our-project-text-id').offsetWidth = $('#our-project-text-id').offsetWidth;
+        $('#our-project-title-id').offsetWidth = $('#our-project-title-id').offsetWidth;
+        $('#our-project-logo-id').offsetWidth = $('#our-project-logo-id').offsetWidth;
+        $('#our-project-text-id').addClass('our-project-text-animation-end');
+        $('#our-project-title-id').addClass('our-project-title-animation-end');
+        $('#our-project-logo-id').addClass('our-project-logo-animation-end');*/
+    }
+    if (isScrolledIntoView($('#our-team-text-id')))
+    {
+        // element is scrolled into view, add animation class
+        $('#our-team-text-id').addClass('our-team-text-animation');
+        $('#our-team-title-id').addClass('our-team-title-animation');
+        $('#our-team-logo-id').addClass('our-team-logo-animation');
+    }
 });
