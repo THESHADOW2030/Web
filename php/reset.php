849dac6af81dbf5b90d69ebb2b348b9dcfbcef06
LUPOSaymon <simone.mija@gmail.com>
2022-05-01 16:42:57 +0200

    Init mail() function

--- php/reset.php
+++ php/reset.php
@@ -16,6 +16,15 @@
 
         if ($line = pg_fetch_array($result, null, PGSQL_ASSOC))
         {
+            //TODO: Ofc it's not working, cause we need a Email server, it's almost impossible doing it locally
+            $token = bin2hex(random_bytes(50));
+            $sql = "INSERT INTO password_reset(email, token) VALUES ('$email', '$token')";
+            $to = $email;
+            $subject = "Reset your password on examplesite.com";
+            $msg = "Hi there, click on this <a href=\"new_password.php?token=" . $token . "\">link</a> to reset your password on our site";
+            $msg = wordwrap($msg,70);
+            $headers = "From: info@infohealth.com";
+            mail($to, $subject, $msg, $headers);
             echo "1";
         }
         else
