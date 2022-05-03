f928d35049a7b0adb547754d38f22ff8e182c78b
THESHADOW2030 <hazem030500@gmail.com>
2022-05-03 16:47:29 +0200

    bello saymon, salutavi sempre. Mi mancherai checkpoint

--- homepage/homepage.php
+++ homepage/homepage.php
@@ -22,6 +22,7 @@
     <link rel="stylesheet" type="text/css" href="../resources/css/index.css">
     <link rel="stylesheet" type="text/css" href="../resources/css/login.css">
     <link rel="stylesheet" type="text/css" href="./homepage.css">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -184,7 +185,41 @@
             <h2>Storico Allenamenti</h2>
         </div>
         <div class="col-sm-6">
-            <a href="#" class="btn btn-dark float-right ">Aggiungi Allenamento</a>
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-dark float-right" data-toggle="modal" data-target="#modalViewAllenamento">
+                Aggiungi Allenamento
+            </button>
+            <div class="modal fade" id="modalViewAllenamento" tabindex="-1" role="dialog" aria-labelledby="modalViewAllenamentoTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLongTitle">Nuovo Allenamento</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="it-datepicker-wrapper theme-dark">
+                                <div class="form-group">
+                                    <label for="date2">Data Allenamento</label>
+                                    <input class="form-control it-date-datepicker" id="date2" type="date">
+                                    <label for="activityInput" style="padding-top: 15px">Attività Svolta</label>
+                                    <input class="form-control" id="calorieBruciateInput" type="text">
+                                    <label for="calorieBruciateInput" style="padding-top: 15px" >Calorie Bruciate</label>
+                                    <input class="form-control" id="passiInput" type="number">
+                                    <label for="passiInput" style="padding-top: 15px">Passi Effettuati</label>
+                                    <input class="form-control" id="passiInput" type="number">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Scarta</button>
+                            <button type="button" class="btn btn-primary">Aggiungi Allenamento</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+           <!-- <a href="#" class="btn btn-dark float-right ">Aggiungi Allenamento</a> -->
         </div>
     </div>
 
@@ -229,7 +264,39 @@
         <h2>Alimenti Mangiati</h2>
     </div>
     <div class="col-sm-6">
-        <a href="#" class="btn btn-dark float-right ">Aggiungi Alimento</a>
+        <button type="button" class="btn btn-dark float-right" data-toggle="modal" data-target="#modalViewAlimento">
+            Aggiungi Alimento
+        </button>
+        <div class="modal fade" id="modalViewAlimento" tabindex="-1" role="dialog" aria-labelledby="modalViewAlimento" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle">Nuovo Alimento</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="it-datepicker-wrapper theme-dark">
+                            <div class="form-group">
+                                <label for="date2">Data Alimento</label>
+                                <input class="form-control it-date-datepicker" id="date2" type="date">
+                                <label for="activityInput" style="padding-top: 15px">Alimento Mangiato</label>
+                                <input class="form-control" id="calorieBruciateInput" type="text">
+                                <label for="calorieBruciateInput" style="padding-top: 15px" >Calorie Alimento</label>
+                                <input class="form-control" id="calorieAssunteInput" type="number">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Scarta</button>
+                        <button type="button" class="btn btn-primary">Aggiungi Alimento</button>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
