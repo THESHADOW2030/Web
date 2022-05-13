c474421b9a0775fe4433dda9c2f4d1d1cc2e3f51
LUPOSaymon <mija.1895592@studenti.uniroma1.it>
2022-05-13 23:20:57 +0200

    Added 'Add Activity Function'

--- jquery/homepage.js
+++ jquery/homepage.js
@@ -21,6 +21,28 @@ function sendAliment()
     return false;
 }
 
+function sendActivity()
+{
+    var form = $("#formActivity");
+    var actionUrl = form.attr('action');
+    var element = $("table-attivita");
+    console.log("sending data...");
+    $.ajax({
+        type: 'POST',
+        url: actionUrl,
+        data: form.serialize(),
+        success: function(response)
+        {
+            $('#table-attivita').load('homepage.php #table-attivita');
+            $('#modalViewAttivita').modal('hide');
+        },
+        error: function(xhr, status, error)
+        {
+            alert(xhr.responseText);
+        }
+    });
+    return false;
+}
 function refreshTable(){
     $('#table-alimenti').load('homepage.php #table-alimenti', function(){
         setTimeout(refreshTable, 5000);
